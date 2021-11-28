02c68d58d1c447b1d7ff84928d268fd54acffb11
SmallY <git@smallyy.cn>
2021-11-28 23:13:09 +0800

    添加注册和登录的 Request 和 Response 类

---
+++ Shared/Models/LoginResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AlgorithmEasy.Shared.Models
+{
+    public class LoginResult
+    {
+        /// <summary>
+        ///     用户是否登陆成功.
+        /// </summary>
+        public bool IsAuthenticated { get; init; }
+
+        /// <summary>
+        ///     登录用户 ID.
+        /// </summary>
+        public string UserId { get; init; }
+
+        /// <summary>
+        ///     此次登录的 sessionId.
+        /// </summary>
+        public Guid SessionId { get; init; }
+    }
+}
---
+++ Shared/Models/RegisterResult.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgorithmEasy.Shared.Models
+{
+    public class RegisterResult
+    {
+        /// <summary>
+        ///     注册是否成功.
+        /// </summary>
+        public bool IsSuccess { get; init; }
+
+        /// <summary>
+        ///     注册用户 ID.
+        /// </summary>
+        public string UserId { get; init; }
+    }
+}
