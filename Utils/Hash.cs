46a0801f411823cb23feb5966da8ee711f1550ac
SmallY <git@smallyy.cn>
2021-12-07 21:13:04 +0800

    添加 SHA256 加密工具类

---
+++ Utils/Hash.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AlgorithmEasy.Shared.Utils
+{
+    public static class Hash
+    {
+        public static byte[] GetSha256(this string str)
+        {
+            HashAlgorithm algorithm = SHA256.Create();
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(str));
+        }
+    }
+}
