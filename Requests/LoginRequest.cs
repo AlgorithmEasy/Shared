35d98744a22f3e043378d51411c204f5e0ade70c
SmallY <git@smallyy.cn>
2021-12-08 17:04:18 +0800

    修改登录、注册的请求和返回类

--- Requests/LoginRequest.cs
+++ Requests/LoginRequest.cs
@@ -1,15 +1,19 @@
-﻿namespace AlgorithmEasy.Shared.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmEasy.Shared.Requests
 {
     public class LoginRequest
     {
         /// <summary>
         ///     登录用户 ID.
         /// </summary>
-        public string UserId { get; init; }
+        [Required(ErrorMessage = "请输入用户名。")]
+        public string UserId { get; set; }
 
         /// <summary>
         ///     登录用户密码, 应进行一次 Sha256 加密.
         /// </summary>
-        public byte[] Password { get; init; }
+        [Required(ErrorMessage = "请输入密码。")]
+        public string Password { get; set; }
     }
 }
---
+++ Requests/RegisterRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmEasy.Shared.Requests
+{
+    public class RegisterRequest
+    {
+        [Required(ErrorMessage = "用户名不能为空。")]
+        [MaxLength(30, ErrorMessage = "用户名不得超过30个字符。")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "昵称不能为空。")]
+        [MaxLength(20, ErrorMessage = "昵称不得超过20个字符。")]
+        public string Nickname { get; set; }
+
+        [Required(ErrorMessage = "密码不能为空。")]
+        public string Password { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+
+        [Required(ErrorMessage = "邮箱不能为空。")]
+        [MaxLength(100)]
+        [EmailAddress(ErrorMessage = "邮箱地址格式不正确。")]
+        public string Email { get; set; }
+    }
+}
