9182d16d1a5973644b674ca36bcfd9dc9ddb32de
SmallY <git@smallyy.cn>
2021-12-08 18:41:03 +0800

    添加返回字符串的 Sha256 方法

--- Utils/Hash.cs
+++ Utils/Hash.cs
@@ -10,5 +10,13 @@ namespace AlgorithmEasy.Shared.Utils
             HashAlgorithm algorithm = SHA256.Create();
             return algorithm.ComputeHash(Encoding.UTF8.GetBytes(str));
         }
+
+        public static string GetSha256String(this string str)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var b in str.GetSha256())
+                stringBuilder.Append(b.ToString("X2"));
+            return stringBuilder.ToString();
+        }
     }
 }
