d194da42ee80285fd8358deb120b950157364d84
SmallY <git@smallyy.cn>
2021-12-10 21:46:31 +0800

    添加项目中心接口请求类

---
+++ Requests/CreateProjectRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmEasy.Shared.Requests
+{
+    public class CreateProjectRequest
+    {
+        [Required]
+        public string ProjectName { get; init; }
+    }
+}
---
+++ Requests/RenameProjectRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmEasy.Shared.Requests
+{
+    public class RenameProjectRequest
+    {
+        [Required]
+        public string OldProjectName { get; init; }
+
+        [Required]
+        public string NewProjectName { get; init; }
+    }
+}
