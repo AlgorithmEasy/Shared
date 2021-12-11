0001e71c9ba34c091de14d273facacf52d4740fe
SmallY <git@smallyy.cn>
2021-12-11 14:55:42 +0800

    添加学习记录子系统请求、返回类

---
+++ Responses/GetPersonalLearningHistoriesResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AlgorithmEasy.Shared.Models;
+
+namespace AlgorithmEasy.Shared.Responses
+{
+    public class GetPersonalLearningHistoriesResponse
+    {
+        public IEnumerable<LearningHistory> LearningHistories { get; set; }
+    }
+}
---
+++ Responses/GetStudentLearningHistoriesResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AlgorithmEasy.Shared.Models;
+
+namespace AlgorithmEasy.Shared.Responses
+{
+    public class GetStudentLearningHistoriesResponse
+    {
+        public IEnumerable<User> Users { get; init; }
+    }
+}
