dcd2f794df95e473242308bc69176b2374d3af48
SmallY <git@smallyy.cn>
2021-12-11 16:12:08 +0800

    添加课程管理子系统请求、返回类

---
+++ Responses/GetCoursesResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AlgorithmEasy.Shared.Models;
+
+namespace AlgorithmEasy.Shared.Responses
+{
+    public class GetCoursesResponse
+    {
+        public IEnumerable<Course> Courses { get; init; }
+    }
+}
