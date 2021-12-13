899aa7479feb84ba9cce9a90902fc039af8bf7e5
SmallY <git@smallyy.cn>
2021-12-13 15:29:11 +0800

    修改项目中心 request 访问权限与数据校验规则

--- Requests/CreateProjectRequest.cs
+++ Requests/CreateProjectRequest.cs
@@ -4,7 +4,8 @@ namespace AlgorithmEasy.Shared.Requests
 {
     public class CreateProjectRequest
     {
-        [Required]
-        public string ProjectName { get; init; }
+        [Required(ErrorMessage = "项目名不得为空。")]
+        [MaxLength(30, ErrorMessage = "项目名称不得超过30字。")]
+        public string ProjectName { get; set; }
     }
 }
--- Requests/RenameProjectRequest.cs
+++ Requests/RenameProjectRequest.cs
@@ -7,7 +7,8 @@ namespace AlgorithmEasy.Shared.Requests
         [Required]
         public string OldProjectName { get; init; }
 
-        [Required]
-        public string NewProjectName { get; init; }
+        [Required(ErrorMessage = "项目名不得为空。")]
+        [MaxLength(30, ErrorMessage = "项目名称不得超过30字。")]
+        public string NewProjectName { get; set; }
     }
 }
