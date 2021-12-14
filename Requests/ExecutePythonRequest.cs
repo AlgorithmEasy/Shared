7eef9c4185200fa68987e120e4c0a099be5850c2
SmallY <git@smallyy.cn>
2021-12-14 23:07:29 +0800

    添加执行 Python 代码请求类

---
+++ Requests/ExecutePythonRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmEasy.Shared.Requests
+{
+    public class ExecutePythonRequest
+    {
+        [Required]
+        public string Code { get; set; }
+    }
+}
