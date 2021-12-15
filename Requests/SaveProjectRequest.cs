6f448c9f39f10c555faaaeaac3cb543be2d9a7ef
SmallY <git@smallyy.cn>
2021-12-15 15:26:05 +0800

    添加保存项目请求类

---
+++ Requests/SaveProjectRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AlgorithmEasy.Shared.Requests
+{
+    public class SaveProjectRequest
+    {
+        public string ProjectName { get; init; }
+
+        public string Workspace { get; init; }
+    }
+}
