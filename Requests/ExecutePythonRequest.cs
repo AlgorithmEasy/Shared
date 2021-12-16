6bb50a1593a72d3de4527b7d3367917bbc637966
SmallY <git@smallyy.cn>
2021-12-16 20:41:51 +0800

    执行代码请求类添加ConnectId字段

--- Requests/ExecutePythonRequest.cs
+++ Requests/ExecutePythonRequest.cs
@@ -5,6 +5,9 @@ namespace AlgorithmEasy.Shared.Requests
     public class ExecutePythonRequest
     {
         [Required]
+        public string ConnectId { get; set; }
+
+        [Required]
         public string Code { get; set; }
     }
 }
